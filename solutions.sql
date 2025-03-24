9755110e906f24810695493bcdf6132d9c4b6aae
Zander <subscription@veryacademy.com>
2025-03-24 20:28:56 +0000

    nc

--- solutions.sql
+++ solutions.sql
@@ -2,5 +2,18 @@
 -- Solutions
 --
 
+SELECT id, name, price, is_active, created_at
+FROM products
+WHERE is_active = TRUE
+ORDER BY created_at DESC;
 
+-- With Alias
 
+SELECT 
+    id AS "Product ID",
+    name AS "Product Name",
+    price AS "Product Price",
+    is_active AS "Product is_active"
+FROM products
+WHERE is_active = TRUE
+ORDER BY created_at DESC;
